8e8fe5c2612c43d8c3fffa7634efff0bf4e5f0fc
Nick Milton <nickdjm@gmail.com>
2021-07-23 14:08:36 -0400

    test: begin fixing tests

--- tests/menus/_common/test-menus.js
+++ tests/menus/_common/test-menus.js
@@ -1,14 +1,16 @@
 export const oneLevelMenu = `
 <header>
-  <nav>
-    <button id="toggle-0">Menu Toggle</button>
-    <ul id="menu-0">
-      <li id="item-1-0-0"><a id="link-1-0-0" href="#">First item</a></li>
-      <li id="item-2-0-0"><a id="link-2-0-0" href="#">Second item</a></li>
-      <li id="item-3-0-0"><a id="link-3-0-0" href="#">Third item</a></li>
-      <li id="item-4-0-0"><a id="link-4-0-0" href="#">Fourth item</a></li>
-      <li id="item-5-0-0"><a id="link-5-0-0" href="#">Fifth item</a></li>
-    </ul>
+  <nav class="navbar">
+    <button id="toggle-0" class="navbar-toggler">Menu Toggle</button>
+    <div class="navbar-collapse">
+      <ul id="menu-0" class="navbar-nav">
+        <li id="item-1-0-0" class="nav-item"><a id="link-1-0-0" class="nav-link" href="#">First item</a></li>
+        <li id="item-2-0-0" class="nav-item"><a id="link-2-0-0" class="nav-link" href="#">Second item</a></li>
+        <li id="item-3-0-0" class="nav-item"><a id="link-3-0-0" class="nav-link" href="#">Third item</a></li>
+        <li id="item-4-0-0" class="nav-item"><a id="link-4-0-0" class="nav-link" href="#">Fourth item</a></li>
+        <li id="item-5-0-0" class="nav-item"><a id="link-5-0-0" class="nav-link" href="#">Fifth item</a></li>
+      </ul>
+    </div>
   </nav>
 </header>
 <main></main>
@@ -16,36 +18,38 @@ export const oneLevelMenu = `
 
 export const twoLevelMenu = `
 <header>
-  <nav>
-    <button id="toggle-0">Menu Toggle</button>
-    <ul id="menu-0">
-      <li id="item-1-0-0"><a id="link-1-0-0" href="#">First item</a></li>
-      <li id="item-2-0-0" class="dropdown">
-        <a id="link-2-0-0" href="#">Second item</a>
-        <ul id="menu-2">
-          <li id="item-2-1-0"><a id="link-2-1-0" href="#">First item</a></li>
-          <li id="item-2-2-0"><a id="link-2-2-0" href="#">Second item</a></li>
-          <li id="item-2-3-0"><a id="link-2-3-0" href="#">Third item</a></li>
-        </ul>
-      </li>
-      <li id="item-3-0-0" class="dropdown">
-        <a id="link-3-0-0" href="#">Third item</a>
-        <ul id="menu-3">
-          <li id="item-3-1-0"><a id="link-3-1-0" href="#">First item</a></li>
-          <li id="item-3-2-0"><a id="link-3-2-0" href="#">Second item</a></li>
-          <li id="item-3-3-0"><a id="link-3-3-0" href="#">Third item</a></li>
-        </ul>
-      </li>
-      <li id="item-4-0-0"><a id="link-4-0-0" href="#">Fourth item</a></li>
-      <li id="item-5-0-0" class="dropdown">
-        <a id="link-5-0-0" href="#">Fifth item</a>
-        <ul id="menu-5">
-          <li id="item-5-1-0"><a id="link-5-1-0" href="#">First item</a></li>
-          <li id="item-5-2-0"><a id="link-5-2-0" href="#">Second item</a></li>
-          <li id="item-5-3-0"><a id="link-5-3-0" href="#">Third item</a></li>
-        </ul>
-      </li>
-    </ul>
+  <nav class="navbar">
+    <button id="toggle-0" class="navbar-toggler">Menu Toggle</button>
+    <div class="navbar-collapse">
+      <ul id="menu-0" class="navbar-nav">
+        <li id="item-1-0-0" class="nav-item"><a id="link-1-0-0" class="nav-link" href="#">First item</a></li>
+        <li id="item-2-0-0" class="nav-item dropdown">
+          <a id="link-2-0-0" class="nav-link dropdown-toggle" href="#">Second item</a>
+          <ul id="menu-2" class="dropdown-menu">
+            <li id="item-2-1-0" class="nav-item"><a id="link-2-1-0" class="dropdown-item" href="#">First item</a></li>
+            <li id="item-2-2-0" class="nav-item"><a id="link-2-2-0" class="dropdown-item" href="#">Second item</a></li>
+            <li id="item-2-3-0" class="nav-item"><a id="link-2-3-0" class="dropdown-item" href="#">Third item</a></li>
+          </ul>
+        </li>
+        <li id="item-3-0-0" class="nav-item dropdown">
+          <a id="link-3-0-0" class="nav-link dropdown-toggle" href="#">Third item</a>
+          <ul id="menu-3" class="dropdown-menu">
+            <li id="item-3-1-0" class="nav-item"><a id="link-3-1-0" class="dropdown-item" href="#">First item</a></li>
+            <li id="item-3-2-0" class="nav-item"><a id="link-3-2-0" class="dropdown-item" href="#">Second item</a></li>
+            <li id="item-3-3-0" class="nav-item"><a id="link-3-3-0" class="dropdown-item" href="#">Third item</a></li>
+          </ul>
+        </li>
+        <li id="item-4-0-0" class="nav-item"><a id="link-4-0-0" class="nav-link" href="#">Fourth item</a></li>
+        <li id="item-5-0-0" class="nav-item dropdown">
+          <a id="link-5-0-0" class="nav-link dropdown-toggle" href="#">Fifth item</a>
+          <ul id="menu-5" class="dropdown-menu">
+            <li id="item-5-1-0" class="nav-item"><a id="link-5-1-0" class="dropdown-item" href="#">First item</a></li>
+            <li id="item-5-2-0" class="nav-item"><a id="link-5-2-0" class="dropdown-item" href="#">Second item</a></li>
+            <li id="item-5-3-0" class="nav-item"><a id="link-5-3-0" class="dropdown-item" href="#">Third item</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </nav>
 </header>
 <main></main>
@@ -53,50 +57,52 @@ export const twoLevelMenu = `
 
 export const fullMenu = `
 <header>
-  <nav>
-    <button id="toggle-0">Menu Toggle</button>
-    <ul id="menu-0">
-      <li id="item-1-0-0"><a id="link-1-0-0" href="#">First item</a></li>
-      <li id="item-2-0-0" class="dropdown">
-        <a id="link-2-0-0" href="#">Second item</a>
-        <ul id="menu-2">
-          <li id="item-2-1-0"><a id="link-2-1-0" href="#">First item</a></li>
-          <li id="item-2-2-0" class="dropdown">
-            <a id="link-2-2-0" href="#">Second item</a>
-            <ul id="menu-2-2">
-              <li id="item-2-2-1"><a id="link-2-2-1" href="#">First item</a></li>
-              <li id="item-2-2-2"><a id="link-2-2-2" href="#">Second item</a></li>
-              <li id="item-2-2-3"><a id="link-2-2-3" href="#">Third item</a></li>
-            </ul>
-          </li>
-          <li id="item-2-3-0" class="dropdown">
-            <a id="link-2-3-0" href="#">Third item</a>
-            <ul id="menu-2-3">
-              <li id="item-2-3-1"><a id="link-2-3-1" href="#">First item</a></li>
-              <li id="item-2-3-2"><a id="link-2-3-2" href="#">Second item</a></li>
-              <li id="item-2-3-3"><a id="link-2-3-3" href="#">Third item</a></li>
-            </ul>
-          </li>
-        </ul>
-      </li>
-      <li id="item-3-0-0" class="dropdown">
-        <a id="link-3-0-0" href="#">Third item</a>
-        <ul id="menu-3">
-          <li id="item-3-1-0"><a id="link-3-1-0" href="#">First item</a></li>
-          <li id="item-3-2-0"><a id="link-3-2-0" href="#">Second item</a></li>
-          <li id="item-3-3-0"><a id="link-3-3-0" href="#">Third item</a></li>
-        </ul>
-      </li>
-      <li id="item-4-0-0"><a id="link-4-0-0" href="#">Fourth item</a></li>
-      <li id="item-5-0-0" class="dropdown">
-        <a id="link-5-0-0" href="#">Fifth item</a>
-        <ul id="menu-5">
-          <li id="item-5-1-0"><a id="link-5-1-0" href="#">First item</a></li>
-          <li id="item-5-2-0"><a id="link-5-2-0" href="#">Second item</a></li>
-          <li id="item-5-3-0"><a id="link-5-3-0" href="#">Third item</a></li>
-        </ul>
-      </li>
-    </ul>
+  <nav class="navbar">
+    <button id="toggle-0" class="navbar-toggler">Menu Toggle</button>
+    <div class="navbar-collapse">
+      <ul id="menu-0" class="navbar-nav">
+        <li id="item-1-0-0" class="nav-item"><a id="link-1-0-0" class="nav-link" href="#">First item</a></li>
+        <li id="item-2-0-0" class="nav-item dropdown">
+          <a id="link-2-0-0" class="nav-link dropdown-toggle" href="#">Second item</a>
+          <ul id="menu-2" class="dropdown-menu">
+            <li id="item-2-1-0" class="nav-item"><a id="link-2-1-0" class="dropdown-item" href="#">First item</a></li>
+            <li id="item-2-2-0" class="nav-item dropdown">
+              <a id="link-2-2-0" class="dropdown-item dropdown-toggle" href="#">Second item</a>
+              <ul id="menu-2-2" class="dropdown-menu">
+                <li id="item-2-2-1" class="nav-item"><a id="link-2-2-1" class="dropdown-item" href="#">First item</a></li>
+                <li id="item-2-2-2" class="nav-item"><a id="link-2-2-2" class="dropdown-item" href="#">Second item</a></li>
+                <li id="item-2-2-3" class="nav-item"><a id="link-2-2-3" class="dropdown-item" href="#">Third item</a></li>
+              </ul>
+            </li>
+            <li id="item-2-3-0" class="nav-item dropdown">
+              <a id="link-2-3-0" class="nav-link dropdown-toggle" href="#">Third item</a>
+              <ul id="menu-2-3" class="dropdown-menu">
+                <li id="item-2-3-1" class="nav-item"><a id="link-2-3-1" class="dropdown-item" href="#">First item</a></li>
+                <li id="item-2-3-2" class="nav-item"><a id="link-2-3-2" class="dropdown-item" href="#">Second item</a></li>
+                <li id="item-2-3-3" class="nav-item"><a id="link-2-3-3" class="dropdown-item" href="#">Third item</a></li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li id="item-3-0-0" class="nav-item dropdown">
+          <a id="link-3-0-0" class="nav-link dropdown-toggle" href="#">Third item</a>
+          <ul id="menu-3" class="dropdown-menu">
+            <li id="item-3-1-0" class="nav-item"><a id="link-3-1-0" class="dropdown-item" href="#">First item</a></li>
+            <li id="item-3-2-0" class="nav-item"><a id="link-3-2-0" class="dropdown-item" href="#">Second item</a></li>
+            <li id="item-3-3-0" class="nav-item"><a id="link-3-3-0" class="dropdown-item" href="#">Third item</a></li>
+          </ul>
+        </li>
+        <li id="item-4-0-0" class="nav-item"><a id="link-4-0-0" class="nav-link" href="#">Fourth item</a></li>
+        <li id="item-5-0-0" class="nav-item dropdown">
+          <a id="link-5-0-0" class="nav-link dropdown-toggle" href="#">Fifth item</a>
+          <ul id="menu-5 dropdown-menu">
+            <li id="item-5-1-0" class="nav-item"><a id="link-5-1-0" class="dropdown-item" href="#">First item</a></li>
+            <li id="item-5-2-0" class="nav-item"><a id="link-5-2-0" class="dropdown-item" href="#">Second item</a></li>
+            <li id="item-5-3-0" class="nav-item"><a id="link-5-3-0" class="dropdown-item" href="#">Third item</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </nav>
 </header>
 <main></main>
